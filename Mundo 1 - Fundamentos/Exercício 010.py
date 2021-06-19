4d1c1990692a78a2ac247b8690d006f97adf77e2
José Roberto Araújo <zeroberto1506@hotmail.com>
2021-06-18 22:12:43 -0300

    Update Exercício 010.py

--- Mundo 1 - Fundamentos/Exercício 010.py
+++ Mundo 1 - Fundamentos/Exercício 010.py
@@ -1,6 +1,11 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar Considere US$ 1,00 = R$ 3,27 
 
 real = float(input("Quantos R$ você tem? R$"))
-dollar = real / 3.7
+# Cotações atualizadas no dia 18/06/2021
+dollar = real / 5.06
+euro = real / 6.01
+pound_sterling = real / 7
 
 print(f"Com R${real:.2f} você pode comprar U${dollar:.2f}")
+print(f"Com R${real:.2f} você pode comprar €{euro:.2f}")
+print(f"Com R${real:.2f} você poe comprar £{pound_sterling:.2f}")

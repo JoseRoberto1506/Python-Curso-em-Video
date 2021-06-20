f986a2b7f5aca36c74f3628cd1a15ff93365d16b
José Roberto Araújo <zeroberto1506@hotmail.com>
2021-06-20 01:00:09 -0300

    Update Exercício 007.py

--- Mundo 1 - Fundamentos/Exercício 007.py
+++ Mundo 1 - Fundamentos/Exercício 007.py
@@ -1,7 +1,7 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 
-note1 = float(input("Digite a primeira nota: "))
-note2 = float(input("Digite a segunda nota: "))
-average = (note1 + note2) / 2
+grade1 = float(input("Digite a primeira nota: "))
+grade2 = float(input("Digite a segunda nota: "))
+average = (grade1 + grade2) / 2
 
-print(f"A média entre {note1} e {note2} é igual a {average:.2f}.")
+print(f"A média entre {grade1} e {grade2} é igual a {average:.2f}.")

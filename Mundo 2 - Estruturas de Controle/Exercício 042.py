b7e025bc482e44dd3428e7b23c3dbe893bc13c7a
José Roberto Araújo <84887543+JoseRoberto1506@users.noreply.github.com>
2021-06-22 00:52:29 -0300

    Update Exercício 042.py

--- Mundo 2 - Estruturas de Controle/Exercício 042.py
+++ Mundo 2 - Estruturas de Controle/Exercício 042.py
@@ -1,4 +1,7 @@
-# Refaça o DESAFIO 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: EQUILÁTERO: todos os lados iguais. ISÓSCELES: dois lados iguais, um diferente. ESCALENO: todos os lados diferentes.
+''' Refaça o DESAFIO 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: 
+EQUILÁTERO: todos os lados iguais. 
+ISÓSCELES: dois lados iguais, um diferente. 
+ESCALENO: todos os lados diferentes.'''
 
 A = float(input("Primeiro segmento: "))
 B = float(input("Segundo segmento: "))

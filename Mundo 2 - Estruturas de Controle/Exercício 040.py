064d21e3ab4579a2a64bc8175d745d5fb66dea01
José Roberto Araújo <84887543+JoseRoberto1506@users.noreply.github.com>
2021-06-22 00:51:12 -0300

    Update Exercício 040.py

--- Mundo 2 - Estruturas de Controle/Exercício 040.py
+++ Mundo 2 - Estruturas de Controle/Exercício 040.py
@@ -1,4 +1,7 @@
-# Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida: Média abaixo de 5.0: REPROVADO. Média entre 5.0 e 6.9: RECUPERAÇÃO. Média 7.0 ou superior: APROVADO.
+''' Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida: 
+Média abaixo de 5.0: REPROVADO. 
+Média entre 5.0 e 6.9: RECUPERAÇÃO. 
+Média 7.0 ou superior: APROVADO.'''
 
 grade1 = float(input("Primeira nota: "))
 grade2 = float(input("Segunda nota: "))

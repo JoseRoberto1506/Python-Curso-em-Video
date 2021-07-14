7d54df47a20c0052854804d299e5da73e52d74b0
José Roberto Araújo <84887543+JoseRoberto1506@users.noreply.github.com>
2021-07-14 00:46:27 -0300

    Update Exercício 058.py

--- Mundo 2 - Estruturas de Controle/Exercício 058.py
+++ Mundo 2 - Estruturas de Controle/Exercício 058.py
@@ -1,4 +1,4 @@
-'''Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos
+''' Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos
 palpites foram necessários para vencer.'''
 
 from random import randint

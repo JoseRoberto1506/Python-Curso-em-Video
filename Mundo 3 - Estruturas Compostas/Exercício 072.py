92a0cb974f2e3cf5e1458b90db19aab7cd2cdb84
José Roberto Araújo <84887543+JoseRoberto1506@users.noreply.github.com>
2021-12-02 15:43:17 -0300

    Update Exercício 072.py

--- Mundo 3 - Estruturas Compostas/Exercício 072.py
+++ Mundo 3 - Estruturas Compostas/Exercício 072.py
@@ -1,5 +1,5 @@
-''' Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e 
-mostrá-lo por extenso.'''
+''' Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) 
+e mostrá-lo por extenso.'''
 
 numbers = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 
            'dezoito', 'dezenove', 'vinte')

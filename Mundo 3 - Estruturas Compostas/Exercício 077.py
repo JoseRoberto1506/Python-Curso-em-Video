fd6029fb3cb596b2bb260f32fa00ea01e2e6795f
José Roberto Araújo <84887543+JoseRoberto1506@users.noreply.github.com>
2021-12-07 15:40:47 -0300

    Update Exercício 077.py

--- Mundo 3 - Estruturas Compostas/Exercício 077.py
+++ Mundo 3 - Estruturas Compostas/Exercício 077.py
@@ -1,7 +1,6 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 
-words = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR',
-'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+words = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
 
 for w in words:
     print(f"\nNa palavra {w} temos ", end='')

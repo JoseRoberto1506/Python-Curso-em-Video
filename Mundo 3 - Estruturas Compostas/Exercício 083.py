b23039f56c7dfb314b8c72bf9bb5daf9dc78c4fa
José Roberto Araújo <84887543+JoseRoberto1506@users.noreply.github.com>
2022-01-05 18:10:51 -0300

    Update Exercício 083.py

--- Mundo 3 - Estruturas Compostas/Exercício 083.py
+++ Mundo 3 - Estruturas Compostas/Exercício 083.py
@@ -1,4 +1,5 @@
-# Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+''' Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos 
+e fechados na ordem correta.'''
 
 expression = str(input("Digite a expressão: "))
 a = []
